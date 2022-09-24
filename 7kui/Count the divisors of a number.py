4f5bc9074658c61c55aee4f1b3da8fdb1fa8ca8d
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2022-09-24 15:58:33 +0300

    7kyu - Count the divisors of a number Update

--- 7kui/Count the divisors of a number.py
+++ 7kui/Count the divisors of a number.py
@@ -11,7 +11,8 @@ Examples (input --> output)
 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
 """
 
-
+"""
+#var1
 def divisors(n):
     divisors = 0
 
@@ -19,3 +20,7 @@ def divisors(n):
         if n % i == 0: divisors += 1
 
     return divisors
+"""
+
+def divisors(n):
+    return len([i for i in range(1, n + 1) if n % i == 0])
