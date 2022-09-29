ae7ed11aff37e28f865dfc5443171bd442d7bea1
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2022-09-29 23:35:47 +0300

    8kyu - Convert boolean values to strings 'Yes' or 'No'.

--- 5kyu/Valid Parentheses.py
+++ 5kyu/Valid Parentheses.py
@@ -16,7 +16,6 @@ Constraints
 Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII characters. Furthermore, the input string may be empty and/or not contain any parentheses at all. Do not treat other forms of brackets as parentheses (e.g. [], {}, <>).
 """
 
-
 def valid_parentheses(string):
     string = ''.join([i for i in string if i in '()'])
     while '()' in string: string = string.replace('()', '')
